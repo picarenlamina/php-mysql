8a0df6c4e1a69d7d1eb4bf174f1b6f15aac13f1d
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2024-04-18 19:11:18 +0200

    Create prueba.php

---
+++ app/prueba.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = "mysql"; // Le host est le nom du service, présent dans le docker-compose.yml
+$dbname = "my-wonderful-website";
+$charset = "utf8";
+$port = "3306";
+
+try {
+    $pdo = new PDO(
+        dsn: "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port",
+        username: "root",
+        password: "super-secret-password",
+    );
+
+    $persons = $pdo->query("SELECT * FROM Persons");
+
+    echo '<pre>';
+    foreach ($persons->fetchAll(PDO::FETCH_ASSOC) as $person) {
+        print_r($person);
+    }
+    echo '</pre>';
+
+} catch (PDOException $e) {
+    throw new PDOException(
+        message: $e->getMessage(),
+        code: (int)$e->getCode()
+    );
+}
